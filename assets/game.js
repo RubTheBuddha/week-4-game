7e604ae51b1b61a4977eb2c501dc668ad62a5b59
rubthebuddha <briellekimberly@Brielles-MacBook-Pro.local>
2017-08-12 14:32:55 -0700

    psuedo

---
+++ assets/game.js
@@ -0,0 +1,23 @@
+
+//  make a start game function
+var crystal1 =rand unique
+var crystal2 =rand unique
+var crystal3 =rand unique
+var crystal4 =rand unique
+var goal = "";
+var points = 0;  // add func points +
+
+var rand=
+
+//---------------
+var wins ="";
+var losses= "";
+var score = "";
+
+
+// create math.random for crystals
+ // Create click events for crytals that displays the value and adds it to
+
+ // if statement that says if above goal you lose is == to goal you increment winss++ 
+ //or losses ++
+ //rune startgame again
