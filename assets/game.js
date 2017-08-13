e106b23d9c3473665389b9f703ce6efa810248b8
rubthebuddha <sean.majure@gmail.com>
2017-08-12 20:29:30 -0700

    Finished

---
+++ assets/game.js
@@ -0,0 +1,77 @@
+
+//  make a start game function
+
+
+//---------------
+var wins ="";
+var losses= "";
+var score = "";
+
+console.log(goal);
+console.log(crystal2);
+console.log(crystal3);
+console.log(crystal4);
+// start game function
+	function startGame(){
+		var crystal1 = Math.floor((Math.random() * 10) + 1);
+		if (crystal1 %2 === 0) {
+			crystal1 == crystal1 +1;
+			console.log(crystal1);
+		}
+			else {
+				crystal1 == crystal1;
+				console.log(crystal1);
+			}
+		
+var crystal2 = Math.floor((Math.random() * 10) + 1);
+var crystal3 = Math.floor((Math.random() * 10) + 1);
+var crystal4 = Math.floor((Math.random() * 10) + 1);
+var goal = Math.floor((Math.random() * 20) + 30);
+var points = 0;  // add func points +
+		}
+var wins ="";
+var losses= "";
+var score = "";
+
+console.log(goal);
+console.log(crystal2);
+console.log(crystal3);
+console.log(crystal4);
+ // Create click events for crytals that displays the value and adds it to
+ window.onload = function {
+
+ $("#crystal1").click(function(){
+    alert("The paragraph was clicked.");
+});
+
+$("#crystal2").click(function(){
+    alert("The paragraph was clicked.");
+});
+
+$("#crystal3").click(function(){
+    alert("The paragraph was clicked.");
+});
+
+$("#crystal4").click(function(){
+    alert("The paragraph was clicked.");
+});
+
+ // if statement that says if above goal you lose is == to goal you increment winss++ 
+ //or losses ++
+ //rune startgame again
+
+
+
+ 	function startGame(){
+var crystalClick1 = Math.floor((Math.random() * 10) + 1);
+var crystalClick2 = Math.floor((Math.random() * 10) + 1);
+var crystalClick3 = Math.floor((Math.random() * 10) + 1);
+var crystalClick4 = Math.floor((Math.random() * 10) + 1);
+var goal = Math.floor((Math.random() * 20) + 30);
+var points = 0;  // add func points +
+console.log(goal);
+console.log(crystalClick1)
+console.log(crystalClick2);
+console.log(crystalClick3);
+console.log(crystalClick4);
+}
